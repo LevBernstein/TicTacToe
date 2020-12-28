d39e635ff3abc19c9ee64a093ba902fcc067976a
LevBernstein <lev2000@me.com>
2020-12-27 23:56:46 -0500

    Switched to fgets and sscanf for taking stdin

--- tictactoe.c
+++ tictactoe.c
@@ -5,6 +5,8 @@
 
 //Author: Lev Bernstein
 //Purpose: Lose to a perfect tic-tac-toe bot.
+//C was probably not the best language for this; Python would've worked much better.
+//I wanted to do something in C, though. No real reason.
 
 
 void printBoard(int board[][3]) {
@@ -46,14 +48,20 @@ int main() {
   int board[3][3] = {{-1,-1,-1}, {-1,-1,-1}, {-1,-1,-1}};
   int playing = 1;
   int valid = 0;
+  int validRow = 0;
+  int validColumn = 0;
+  int pRow = -1;
+  int pColumn = -1;
   int turn = 0;
   char row[10];
   char column[10];
+  char temp[255];
   
   while (first == -1) {
     printf("Would you like to go first? Please type 'yes' or 'no'.\n");
-    scanf("%s", answer);
-    answer[0] = tolower(answer[0]); //In case someone types in 'Yes' or 'No', this shifts the first letter down to lowercase. Anyone typing in all caps should stop doing so.
+    fgets(temp, 256, stdin);
+    sscanf(temp, "%s\n", answer);
+    answer[0] = tolower(answer[0]); //In case someone types in 'Yes' or 'No', this shifts the first letter down to lowercase. Anyone typing in all caps should simply stop doing so.
     if (strcmp(answer, yes) == 0) {
       first = 1;
     }
@@ -73,8 +81,37 @@ int main() {
     if (turn == 1) {
       valid = 0;
       while (!valid) {
+	pRow = -1;
+	pColumn = -1;
 	printf("It is your move. Please type in the cell where you'd like to place your X. You can write something like 'middle row right column' or 'bottom row left column', etc.\n");
-	scanf("%s row %s column", row, column);
+	fflush(stdin);
+	fgets(temp, 256, stdin);
+	sscanf(temp, "%s row %s column", row, column);
+	fflush(stdin);
+	valid = 1;
+	if (strcmp(row, top) == 0) {
+	  pRow = 0;
+	}
+	else if (strcmp(row, middle) == 0) {
+	  pRow = 1;
+	}
+	else if (strcmp(row, bottom) == 0) {
+	  pRow = 2;
+	}
+	if (pRow != -1) {
+	  if (strcmp(column, left) == 0) {
+	    pColumn = 0;
+	  }
+	  else if (strcmp(column, middle) == 0) {
+	    pColumn = 1;
+	  }
+	  else if (strcmp(column, right) == 0) {
+	    pColumn = 2;
+	  }
+	}
+	if (pRow == -1 || pColumn == -1) {
+	  valid = 0;
+	}
 	
       }
     }
